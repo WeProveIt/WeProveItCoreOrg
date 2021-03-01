0a972038c8750053ed98b7b5a9414aed190c17d2
Lovya Grace <lovya@qburst.com>
2021-03-02 00:00:27 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeProveItCoreOrg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
